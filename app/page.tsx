14fc4fe98dc0b308701f66553149b2716358353b
RyoAiba <tingensai.kudasai@gmail.com>
2026-05-31 09:17:02 +0900

    テスト3

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 
 export default function Page() {
   useEffect(() => {
-    alert(`standalone=${window.navigator.standalone}`)
+    alert(`standalone=${(window.navigator as Navigator & { standalone?: boolean }).standalone}`)
   }, [])
   return <div>Hello</div>
 }
