cb7ccba190056c5751cc3dc9b7f8a26ba2cf2390
RyoAiba <tingensai.kudasai@gmail.com>
2026-05-31 09:14:27 +0900

    テスト2

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+"use client"
+import { useEffect } from "react"
+
 export default function Page() {
+  useEffect(() => {
+    alert(`standalone=${window.navigator.standalone}`)
+  }, [])
   return <div>Hello</div>
 }
