3421f3949f6c5bd8242540fafbe036766d4bcc70
RyoAiba <tingensai.kudasai@gmail.com>
2026-06-05 13:04:19 +0900

    テスト４

--- app/page.tsx
+++ app/page.tsx
@@ -141,6 +141,9 @@ export default function Dashboard() {
               dataKey="value"
               startAngle={90}
               endAngle={-270}
+              animationBegin={0}
+              animationDuration={800}
+              isAnimationActive={true}
             >
               <Cell className="fill-brand-500" />
               <Cell className="fill-gray-200" />
